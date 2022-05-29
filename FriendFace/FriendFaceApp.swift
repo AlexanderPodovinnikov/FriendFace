b0169dc9e6b0b936e02f8249d71326b566aaff30
AlexanderPodovinnikov <art.iq@hotmail.com>
2022-05-29 22:20:27 +0300

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Alex Po on 29.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
